24cae9df48714eb3bb4031a1f59042714b4d0805
Dharmik_403 <hingudharmik4@gmail.com>
2022-07-12 16:17:46 +0530

    change gradient colors

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,9 +2,9 @@ import trollFace from '../images/troll_face.png';
 
 export default function Header() {
 	return (
-		<header className='md:h-28 lg:p-6 lg:gap-6 lg:h-24 flex items-center gap-8 flex-row h-36 p-8 bg-gradient-to-r from-teal-500 to-amber-400'>
+		<header className='h-44 flex items-center py-8 px-12 gap-8 bg-gradient-to-r from-algae to-grass shadow-2xl md:h-44 lg:h-28 lg:px-20'>
 			<img className='aspect-square h-full' src={trollFace} alt='troll face' />
-			<h1 className='lg:text-4xl text-white font-semibold text-6xl font-sans tracking-tightest'>
+			<h1 className='lg:text-4xl text-white font-semibold text-5xl font-sans tracking-tightest'>
 				Memeracle
 			</h1>
 		</header>
