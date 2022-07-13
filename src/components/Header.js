220a7f4ccd7464c3e8c219cd4d878e928ece223e
Dharmik_403 <hingudharmik4@gmail.com>
2022-07-13 15:29:58 +0530

    imporve Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,7 @@ import trollFace from '../images/troll_face.png';
 
 export default function Header() {
 	return (
-		<header className='h-44 flex items-center py-8 px-12 gap-8 bg-gradient-to-r from-algae to-grass shadow-2xl md:h-44 lg:h-28 lg:px-20'>
+		<header className='h-44 flex items-center py-8 px-12 gap-8 bg-gradient-to-r from-algae to-grass shadow-2xl md:h-40 lg:h-28 lg:px-44'>
 			<img className='aspect-square h-full' src={trollFace} alt='troll face' />
 			<h1 className='lg:text-4xl text-white font-semibold text-5xl font-sans tracking-tightest'>
 				Memeracle
