e32a54527b7ed4bb4c284afc783d00f1f3d457b6
Dharmik_403 <hingudharmik4@gmail.com>
2022-07-13 15:33:40 +0530

    basic working version complete

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -1,26 +1,45 @@
-import { data } from 'autoprefixer';
 import { useEffect, useState } from 'react';
 import InputBox from './InputBox';
 import MemeImg from './MemeImg';
+import DownloadBtn from './DownloadBtn';
+import RandomImg from './RandomImg';
 
 export default function Meme() {
-	const [memeUrl, setMemeUrl] = useState('');
+	const [memes, setMemes] = useState([]);
+	const [currMeme, setCurrMeme] = useState({});
+	const [topText, setTopText] = useState('Top Text');
+	const [bottomText, setBottomText] = useState('Bottom Text');
 
 	useEffect(() => {
 		fetch('https://api.imgflip.com/get_memes')
 			.then(data => data.json())
 			.then(({ data }) => {
-				setMemeUrl(data.memes[0]);
+				setMemes(data.memes);
+				setCurrMeme(
+					data.memes[Math.floor(Math.random() * data.memes.length + 1)]
+				);
 			});
 	}, []);
 
 	return (
-		<section className='my-20 mx-12 lg:mx-20'>
-			<form className='flex flex-col gap-8 w-full lg:flex-row'>
-				<InputBox placeholder='Top Text' />
-				<InputBox placeholder='Bottom Text' />
-			</form>
-			<MemeImg imgUrl={memeUrl} />
+		<section className='py-20 mx-12 flex gap-14 flex-col lg:flex-row-reverse lg:mx-44'>
+			<div className='flex flex-col gap-8 flex-1'>
+				<form className='flex flex-col gap-8 h-min'>
+					<InputBox
+						placeholder='Top Text'
+						text={topText}
+						setText={setTopText}
+					/>
+					<InputBox
+						placeholder='Bottom Text'
+						text={bottomText}
+						setText={setBottomText}
+					/>
+				</form>
+				<DownloadBtn />
+				<RandomImg memes={memes} setCurrMeme={setCurrMeme} />
+			</div>
+			<MemeImg memeData={currMeme} topText={topText} bottomText={bottomText} />
 		</section>
 	);
 }
--- src/components/MemeImg.js
+++ src/components/MemeImg.js
@@ -1,13 +1,24 @@
 import Draggable from 'react-draggable';
 
-export default function MemeImg({ imgUrl }) {
-	console.log(imgUrl);
+export default function MemeImg({ memeData, topText, bottomText }) {
+	// console.log(memeData);
 	return (
-		<div className='relative'>
+		<div id='meme' className='relative flex-1'>
+			<img
+				src={memeData.url}
+				alt={memeData.name}
+				className='w-full aspect-square'
+			/>
 			<Draggable bounds='parent'>
-				<div className='w-fit'>Drag me</div>
+				<div className='top-5 left-1/3 cursor-grab meme-text text-4xl font-mono text-white w-fit absolute'>
+					{topText}
+				</div>
 			</Draggable>
-			<img src={imgUrl.url} alt={imgUrl.name} className='lg:w-1/3' />
+			<Draggable bounds='parent'>
+				<div className='bottom-5 left-1/3 cursor-grab meme-text text-4xl font-mono text-white w-fit absolute'>
+					{bottomText}
+				</div>
+			</Draggable>
 		</div>
 	);
 }
