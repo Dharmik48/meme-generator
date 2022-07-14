311d252d5da2c9c26c302dccae27836caa1bebbc
Dharmik_403 <hingudharmik4@gmail.com>
2022-07-14 13:10:19 +0530

    change colors

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,19 +4,19 @@ import { faGithub, faTwitter } from '@fortawesome/free-brands-svg-icons';
 
 export default function Footer() {
 	return (
-		<footer className='text-lg flex justify-between py-8 px-12 bg-gradient-to-r from-algae to-grass shadow-2xl lg:px-44'>
+		<footer className='text-lg flex justify-between py-8 px-12 bg-algae shadow-2xl lg:px-44'>
 			<p>
 				Made with <FontAwesomeIcon icon={faHeart} color='red' /> by Dharmik
 			</p>
 			<ul className='flex gap-4'>
 				<li>
 					<a target='_blank' href='https://github.com/Dharmik48/meme-generator'>
-						<FontAwesomeIcon icon={faGithub} size='lg' />
+						<FontAwesomeIcon icon={faGithub} size='xl' color='white' />
 					</a>
 				</li>
 				<li>
 					<a target='_blank' href='https://twitter.com/frontendilia'>
-						<FontAwesomeIcon icon={faTwitter} size='lg' />
+						<FontAwesomeIcon icon={faTwitter} size='xl' color='white' />
 					</a>
 				</li>
 			</ul>
