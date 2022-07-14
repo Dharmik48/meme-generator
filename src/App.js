ec45cb3822da7a889427cfcc93bb2daf2b9eea3f
Dharmik_403 <hingudharmik4@gmail.com>
2022-07-14 11:28:54 +0530

    Add footer

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Header from './components/Header';
 import Meme from './components/Meme';
+import Footer from './components/Footer';
 
 export default function App() {
 	return (
-		<div className='dark:bg-palenight min-h-screen w-screen'>
+		<div className='flex flex-col justify-between dark:bg-palenight min-h-screen w-screen'>
 			<Header />
 			<Meme />
+			<Footer />
 		</div>
 	);
 }
