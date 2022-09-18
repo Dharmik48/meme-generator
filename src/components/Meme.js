7d75e66892fc22ed60e09040c29920f51159b923
Gulam Anas <g.anas786@gmail.com>
2022-09-18 16:22:15 +0530

    refactored line align test

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -11,7 +11,13 @@ export default function Meme({ memes, currMeme, setCurrMeme }) {
     const [Text4, setText4] = useState("Text4");
     const [Text5, setText5] = useState("Text5");
 
-    let allInputs = [<InputBox text={Text1} setText={setText1}/>,<InputBox text={Text2} setText={setText2}/>,<InputBox text={Text3} setText={setText3}/>,<InputBox text={Text4} setText={setText4}/>,<InputBox text={Text5} setText={setText5}/>]
+    let allInputs = [
+        <InputBox text={Text1} setText={setText1} />,
+        <InputBox text={Text2} setText={setText2} />,
+        <InputBox text={Text3} setText={setText3} />,
+        <InputBox text={Text4} setText={setText4} />,
+        <InputBox text={Text5} setText={setText5} />
+    ]
 
     useEffect(() => {
         Object.keys(currMeme).length === 0 &&
@@ -22,7 +28,7 @@ export default function Meme({ memes, currMeme, setCurrMeme }) {
         <section className="py-20 mx-12 flex gap-14 flex-col lg:flex-row-reverse lg:mx-44">
             <div className="flex flex-col gap-8 flex-1">
                 <form className="flex flex-col gap-8 h-min">
-                    {allInputs.slice(0,currMeme.box_count).map((input)=>(
+                    {allInputs.slice(0, currMeme.box_count).map((input) => (
                         input
                     ))}
                 </form>
